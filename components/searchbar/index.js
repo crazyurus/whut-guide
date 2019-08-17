f1cef436dacf71ece468585b8e4b4cb29f01960d
Crazy Urus <crazyurus@vip.qq.com>
2019-08-18 00:41:46 +0800

    搜索条件空判断

--- components/searchbar/index.js
+++ components/searchbar/index.js
@@ -9,6 +9,14 @@ Component({
       const type = e.type;
       const wd = type === 'tap' ? word : e.detail.value;
 
+      if (wd.trim() === '') {
+        wx.showToast({
+          title: '搜索关键词必填',
+          icon: 'none'
+        });
+        return;
+      }
+
       this.triggerEvent('search', { value: wd });
     },
     change(e) {
