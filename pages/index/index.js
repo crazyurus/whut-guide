81cab1f49b93925e72c849e2381260b80de7c7fc
廖星 <liaoxing.lx@bytedance.com>
2019-09-23 11:31:01 +0800

    首页数据请求和缓存

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,25 @@
-import categories from '../../data/category.js';
-
 const app = getApp()
 
 Page({
   data: {
-    categories
+    categories: wx.getStorageSync('categories')
+  },
+  onLoad() {
+    const colors = ['#3390b9', '#4fabd2', '#80c5e4', '#8cceed'];
+    
+    app.request.get('https://test-api-iwut.wutnews.net/api/v1/category').then(response => {
+      const categories = response.map((category, index) => {
+        return {
+          ...category,
+          color: colors[Math.floor(index / 2)]
+        }
+      });
+
+      wx.setStorageSync('categories', categories);
+      this.setData({
+        categories,
+      });
+    }, false);
   },
   onShareAppMessage() {
     return {};
