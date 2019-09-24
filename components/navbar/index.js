15a9bbe55f32cd74cb1c6310661f9e478ec54095
廖星 <liaoxing.lx@bytedance.com>
2019-09-24 17:31:50 +0800

    支持添加到桌面

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -23,13 +23,20 @@ Component({
       this.setData({
         statusBarHeight: systemInfo.statusBarHeight,
         navBarHeight: menuButton.height + (menuButton.top - systemInfo.statusBarHeight) * 2,  
-        showBack: getCurrentPages().length > 1
+        showBack: getCurrentPages().length > 1,
+        isMobileQQ: systemInfo.AppPlatform === 'qq'
       });
     }
   },
   methods: {
     back() {
       if (this.data.showBack) wx.navigateBack();
+    },
+    title() {
+      if (this.data.showBack) this.back();
+      else if (this.data.isMobileQQ) {
+        wx.saveAppToDesktop();
+      }
     }
   }
 });
